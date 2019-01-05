798e0b3dbeb68bfbb630894b541fdcaf5ad9c8fb
Max Quirk <maxquirk@Maxs-MacBook-Pro.local>
2019-01-05 10:25:32 +1000

    privacy policy

--- app.py
+++ app.py
@@ -25,6 +25,109 @@ app.debug = True
 all_course_assessment = []
 port = int(os.environ.get('PORT', 5000))
 
+
+def formatDate(date):
+    return dateparser.parse(date)
+
+def getProfileID(course_code):
+    base_url = 'http://www.uq.edu.au/study/course.html?course_code=%s' \
+        % course_code
+    soup = helpers.get_soup(base_url)
+    if soup is None or soup.find(id="course-notfound"):
+        return None
+    profileID = soup.find(class_='profile-available')['href'].split('=')[-1]
+    return profileID
+
+
+def getAssessments(profileID, course_code):
+    ECP_url = "https://www.courses.uq.edu.au/student_section_loader.php?section=5&profileId=%s" % profileID
+
+    # gets all tables on the page
+    all_tables = pd.read_html(ECP_url, match='Assessment Task')
+    # gets table containing desired information
+    table = all_tables[2]
+    table_length = table.shape[0] - 1
+    print(table)
+    assessments = []
+    i = 1
+
+    while i <= table_length:
+        name = table.at[i, 0]
+        print(splitName)
+        due_date = table.at[i, 1]
+        weighting = table.at[i, 2]
+        learning_obj = table.at[i, 3]
+
+        assessment = {
+            'course_code': course_code,
+            'name': splitName(name),
+            'due_date': due_date,
+            'weighting': weighting,
+            'learning_obj': learning_obj
+        }
+        assessments.append(assessment)
+
+        i += 1
+
+    return assessments
+
+
+def splitName(name):  # Separates assessment type from assessment name
+    for index, letter in enumerate(name):
+        try:
+            if letter.islower() and name[index + 1].isupper() \
+                    or letter == ')' and name[index + 1].isupper():
+                return name[index+1:]
+        except IndexError:
+            return name
+    return name
+
+
+def makeHTML(all_course_assessments):
+    html = ''
+    html += '<div class="row">'
+    columns = len(all_course_assessments)
+
+    for course in all_course_assessments:
+        html += '<div class="column-%s">' % columns
+        html += '<h2 class="course-code">' + \
+            course[0].get('course_code') + '</h2>'
+        for assessment in course:
+            html += '<h4 class="assessment-name"><strong>' + \
+                assessment.get('name') + '</h4></strong>'
+            html += '<p class="due-date">' + \
+                assessment.get('due_date') + '</p>'
+            html += '<p class="weighting">' + \
+                assessment.get('weighting') + '</p>'
+        html += '</div>'
+    html += '</div>'
+    return html
+
+def makeChronologicalHTML(all_course_assessments):
+    formatable_assessments = []
+    for course in all_course_assessment:
+        for assessment in course:
+            if formatDate(assessment.get('due_date')) != None:
+                formatable_assessments.append(assessment)
+
+    formatable_assessments = sorted(formatable_assessments, key=lambda k: formatDate(k['due_date'])) 
+
+    html = ''
+    for assessment in formatable_assessments:
+        title = '%s - %s' % (assessment.get('course_code').upper(), assessment.get('name'))
+        html += '<tr>'
+        html += '<td>%s</td>' % title
+        html += '<td>%s</td>' % assessment.get('due_date')
+        html += '<td>%s</td>' % assessment.get('weighting')
+        html += '<td>%s</td>' % assessment.get('learning_obj')
+        html += '</tr>'
+    
+
+    return html
+
+def calExport(calendar, event):
+    event = calendar.events().insert(calendarId='primary', body=event).execute()
+
 @app.route("/", methods=['GET', 'POST'])
 def hello():
     print(request.method)
@@ -146,6 +249,9 @@ def oauth2callback():
 
   return flask.redirect('export')
 
+@app.route('/privacy')
+def privacy():
+    return render_template('privacy.html')
 
 def credentials_to_dict(credentials):
   return {'token': credentials.token,
@@ -157,106 +263,3 @@ def credentials_to_dict(credentials):
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=port)
-
-def formatDate(date):
-    return dateparser.parse(date)
-
-
-def getProfileID(course_code):
-    base_url = 'http://www.uq.edu.au/study/course.html?course_code=%s' \
-        % course_code
-    soup = helpers.get_soup(base_url)
-    if soup is None or soup.find(id="course-notfound"):
-        return None
-    profileID = soup.find(class_='profile-available')['href'].split('=')[-1]
-    return profileID
-
-
-def getAssessments(profileID, course_code):
-    ECP_url = "https://www.courses.uq.edu.au/student_section_loader.php?section=5&profileId=%s" % profileID
-
-    # gets all tables on the page
-    all_tables = pd.read_html(ECP_url, match='Assessment Task')
-    # gets table containing desired information
-    table = all_tables[2]
-    table_length = table.shape[0] - 1
-    print(table)
-    assessments = []
-    i = 1
-
-    while i <= table_length:
-        name = table.at[i, 0]
-        print(splitName)
-        due_date = table.at[i, 1]
-        weighting = table.at[i, 2]
-        learning_obj = table.at[i, 3]
-
-        assessment = {
-            'course_code': course_code,
-            'name': splitName(name),
-            'due_date': due_date,
-            'weighting': weighting,
-            'learning_obj': learning_obj
-        }
-        assessments.append(assessment)
-
-        i += 1
-
-    return assessments
-
-
-def splitName(name):  # Separates assessment type from assessment name
-    for index, letter in enumerate(name):
-        try:
-            if letter.islower() and name[index + 1].isupper() \
-                    or letter == ')' and name[index + 1].isupper():
-                return name[index+1:]
-        except IndexError:
-            return name
-    return name
-
-
-def makeHTML(all_course_assessments):
-    html = ''
-    html += '<div class="row">'
-    columns = len(all_course_assessments)
-
-    for course in all_course_assessments:
-        html += '<div class="column-%s">' % columns
-        html += '<h2 class="course-code">' + \
-            course[0].get('course_code') + '</h2>'
-        for assessment in course:
-            html += '<h4 class="assessment-name"><strong>' + \
-                assessment.get('name') + '</h4></strong>'
-            html += '<p class="due-date">' + \
-                assessment.get('due_date') + '</p>'
-            html += '<p class="weighting">' + \
-                assessment.get('weighting') + '</p>'
-        html += '</div>'
-    html += '</div>'
-    return html
-
-def makeChronologicalHTML(all_course_assessments):
-    formatable_assessments = []
-    for course in all_course_assessment:
-        for assessment in course:
-            if formatDate(assessment.get('due_date')) != None:
-                formatable_assessments.append(assessment)
-
-    formatable_assessments = sorted(formatable_assessments, key=lambda k: formatDate(k['due_date'])) 
-
-    html = ''
-    for assessment in formatable_assessments:
-        title = '%s - %s' % (assessment.get('course_code').upper(), assessment.get('name'))
-        html += '<tr>'
-        html += '<td>%s</td>' % title
-        html += '<td>%s</td>' % assessment.get('due_date')
-        html += '<td>%s</td>' % assessment.get('weighting')
-        html += '<td>%s</td>' % assessment.get('learning_obj')
-        html += '</tr>'
-    
-
-    return html
-
-def calExport(calendar, event):
-    event = calendar.events().insert(calendarId='primary', body=event).execute()

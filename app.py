8108e20c0c735aae45786686b02550a8055df56f
Tom Quirk <tomquirkacc@gmail.com>
2019-02-25 10:34:58 +1100

    add logger; attempt script ting

--- app.py
+++ app.py
@@ -1,27 +1,28 @@
 import os
-import asyncio
-import sys
-import csv
-from datetime import datetime, timedelta
+from datetime import timedelta
+import json
 
 import flask
 from flask import Flask, render_template, request, jsonify, redirect, url_for
 from flask.cli import with_appcontext
-
 import pandas as pd
 import dateparser
-
 import google.oauth2.credentials
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
 
 from database import Db
+from logger import get_logger
 import settings
+import util
+
+_LOG = get_logger("app")
 
 
 app = Flask(__name__)
 app.debug = True
 app.secret_key = settings.APP_SECRET
+PORT = int(os.environ.get("PORT", 5000))
 
 
 def get_db():
@@ -30,7 +31,7 @@ def get_db():
     current application context.
     """
     if not hasattr(flask.g, "db"):
-        flask.g.db = Db(detailed=False)
+        flask.g.db = Db()
         flask.g.db.connect(settings.DATABASE_URL)
 
     return flask.g.db
@@ -43,59 +44,22 @@ def close_db(error):
         flask.g.db.close()
 
 
-port = int(os.environ.get("PORT", 5000))
-
-
-def formatDate(date):
-    print(date)
-    # check if date contains no digits
-    if not any(char.isdigit() for char in date):
-        return None
-
-    dates = date.split("-")
-    if len(dates) > 1:
-        date1 = dateparser.parse(dates[0])
-        date2 = dateparser.parse(dates[1])
-        if date1 and date2:
-            if date1.date() == date2.date():
-                date = dates[0]
-                return dateparser.parse(date)
-    date = dateparser.parse(date)
-    return date
-
-
 def getProfileID(course_code):
-    print("getting profile id...")
-    query = "SELECT course_profile_id FROM course WHERE course_code = '%s'" % (
-        course_code.upper()
-    )
-    profileID = get_db().select(query)
+    query = """
+    SELECT course_profile_id FROM course WHERE course_code = %s
+    """
+    profileID = get_db().select(query, data=(course_code.upper(),))
     if profileID:
-        print("finished getting profile id")
         return profileID[0][0]
-    # base_url = 'http://www.uq.edu.au/study/course.html?course_code=%s' \
-    #     % course_code
-    # soup = helpers.get_soup(base_url)
-    # if soup is None or soup.find(id="course-notfound"):
-    #     return None
-    # profileID = soup.find(class_='profile-available')['href'].split('=')[-1]
-    # return profileID
 
 
 def getAssessments(profileID, course_code):
-    # ECP_url = "https://www.courses.uq.edu.au/student_section_loader.php?section=5&profileId=%s" % profileID
-    print("getting assessments...")
     query = """
             SELECT assessment_name, due_date, weighting, learning_obj 
             FROM course_assessment
-            WHERE course_code = '%s'
-            """ % (
-        course_code.upper()
-    )
-    unformatted_assessments = get_db().select(query)
-    print("here")
-    print(unformatted_assessments)
-    print(not unformatted_assessments)
+            WHERE course_code = %s
+            """
+    unformatted_assessments = get_db().select(query, data=(course_code.upper(),))
     if not unformatted_assessments:
         return None
 
@@ -116,118 +80,11 @@ def getAssessments(profileID, course_code):
         }
 
         assessments.append(assessment)
-    # ECP_url = 'https://www.courses.uq.edu.au/student_section_loader.php?section=5&profileId=%s' % profileID
-    # # gets all tables on the page
-    # all_tables = pd.read_html(ECP_url, match='Assessment Task')
-    # # gets tables containing desired information
-    # all_tables = all_tables[2:]
-    # assessments = []
-    # for table in all_tables:
-    #     table_length = table.shape[0] - 1
-    #     i = 1
-
-    #     while i <= table_length:
-
-    #         name = table.at[i, 0]
-    #         print(splitName(name))
-    #         due_date = table.at[i, 1]
-    #         weighting = table.at[i, 2]
-    #         learning_obj = table.at[i, 3]
-
-    #         assessment = {
-    #             'course_code': course_code,
-    #             'name': splitName(name),
-    #             'due_date': due_date,
-    #             'weighting': weighting,
-    #             'learning_obj': learning_obj
-    #         }
-    #         assessments.append(assessment)
-
-    #         i += 1
-
-    print("finished getting assessments")
-
     return assessments
-
-
-def splitName(name):  # Separates assessment type from assessment name
-    for index, letter in enumerate(name):
-        try:
-            if (
-                letter.islower()
-                and name[index + 1].isupper()
-                or letter == ")"
-                and name[index + 1].isupper()
-            ):
-                return name[index + 1 :]
-        except IndexError:
-            return name
-    return name
-
-
-def makeHTML(all_course_assessments):
-    print("making html...")
-    html = ""
-    html += '<div class="row">'
-    columns = len(all_course_assessments)
-
-    for course in all_course_assessments:
-        html += '<div class="column-%s">' % columns
-        html += '<h2 class="course-code">' + course[0].get("course_code") + "</h2>"
-        for assessment in course:
-            html += (
-                '<h4 class="assessment-name"><strong>'
-                + assessment.get("name")
-                + "</h4></strong>"
-            )
-            html += '<p class="due-date">' + assessment.get("due_date") + "</p>"
-            html += '<p class="weighting">' + assessment.get("weighting") + "</p>"
-        html += "</div>"
-    html += "</div>"
-    print("finished making html")
-    return html
-
-
-def makeChronologicalHTML(all_course_assessments):
-    print("making chronological html...")
-    formatable_assessments = []
-    for course in all_course_assessment:
-        for assessment in course:
-            print(assessment)
-            if formatDate(assessment.get("due_date")) != None:
-                formatable_assessments.append(assessment)
-    print(formatable_assessments)
-    if formatable_assessments:
-        formatable_assessments = sorted(
-            formatable_assessments, key=lambda k: formatDate(k["due_date"])
-        )
-        html = ""
-        for assessment in formatable_assessments:
-            title = "%s - %s" % (
-                assessment.get("course_code").upper(),
-                assessment.get("name"),
-            )
-            html += "<tr>"
-            html += "<td>%s</td>" % title
-            html += "<td>%s</td>" % assessment.get("due_date")
-            html += "<td>%s</td>" % assessment.get("weighting")
-            html += "<td>%s</td>" % assessment.get("learning_obj")
-            html += "</tr>"
-
-        print("finished making chronological html")
-        return html
-
-    return ""
-
-
-def calExport(calendar, payload):
-    print(payload)
-    calendar.events().insert(calendarId="primary", body=payload).execute()
 
 
 @app.route("/", methods=["GET", "POST"])
 def hello():
-    print(request.method)
     if request.method == "GET":
         return render_template("title.html")
 
@@ -248,28 +105,26 @@ def hello():
     if course_5 is not None and course_5 != "":
         all_courses.append(course_5)
 
-    print(all_courses)
-
+    all_course_assessment = []
     for course_code in all_courses:
         profileID = getProfileID(course_code)
-        print("legit here")
-        print(profileID)
         if profileID == 0:
             error_message = '<p style="color: red">Error: One of the courses you entered was invalid. Please try again</p>'
             all_courses = []
             return render_template("title.html", error_message=error_message)
-        all_course_assessment = []
+
         assessments = getAssessments(profileID, course_code)
         if assessments:
             all_course_assessment.append(assessments)
 
-    by_course_as_html = makeHTML(all_course_assessment)
-    chronological_html = makeChronologicalHTML(all_course_assessment)
+    by_course_as_html = util.makeHTML(all_course_assessment)
+    chronological_html = util.makeChronologicalHTML(all_course_assessment)
 
     return render_template(
         "citation.html",
         by_course_as_html=by_course_as_html,
         chronological_html=chronological_html,
+        state={assessments: json.dumps(all_course_assessment)},
     )
 
 
@@ -285,14 +140,13 @@ def export():
 
     for course_assessments in all_course_assessment:
         for assessment in course_assessments:
-            print(assessment)
             course = assessment["course_code"].upper()
             assessment_name = assessment["name"]
             weighting = assessment["weighting"]
             learning_obj = assessment["learning_obj"]
-            due_date = formatDate(assessment["due_date"])
+            due_date = util.formatDate(assessment["due_date"])
 
-            if formatDate(assessment["due_date"]) != None:
+            if util.formatDate(assessment["due_date"]) != None:
                 title = "%s - %s" % (course, assessment_name)
                 description = "Weighting: %s \nLearning objectives: %s" % (
                     weighting,
@@ -309,7 +163,7 @@ def export():
                     "end": {"dateTime": end.A1(), "timeZone": "Australia/Brisbane"},
                 }
 
-                calExport(calendar, event)
+                util.calExport(calendar, event)
 
     return jsonify(
         {
@@ -350,7 +204,7 @@ def oauth2callback():
     # ACTION ITEM: In a production app, you likely want to save these
     #              credentials in a persistent database instead.
     credentials = flow.credentials
-    flask.session["credentials"] = credentials_to_dict(credentials)
+    flask.session["credentials"] = util.credentials_to_dict(credentials)
 
     return render_template("oauth2_success.html")
 
@@ -360,22 +214,11 @@ def privacy():
     return render_template("privacy.html")
 
 
-def credentials_to_dict(credentials):
-    return {
-        "token": credentials.token,
-        "refresh_token": credentials.refresh_token,
-        "token_uri": credentials.token_uri,
-        "client_id": credentials.client_id,
-        "client_secret": credentials.client_secret,
-        "scopes": credentials.scopes,
-    }
-
-
 @app.route("/.well-known/acme-challenge/gLguitC8-PJzAcAHOGHvlK_q_71gSJQQcB_xdpVzENQ")
 def verify():
     return "gLguitC8-PJzAcAHOGHvlK_q_71gSJQQcB_xdpVzENQ.jjDWwuoR6VaOPJCS3RY3gQwgiU77PLzwzYN8i2Os4Zs"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=PORT)
 
